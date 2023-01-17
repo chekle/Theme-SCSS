b9c0994ab1df2461ed9f5a5531240f3f79699e26
Cheyne <chekle@users.noreply.github.com>
2023-01-17 15:47:12 +1300

    adding main tag

--- single.php
+++ single.php
@@ -7,29 +7,31 @@
 get_header();
 ?>
 
-<section id="blog-single" class="margin">
-	<div class="container">
-		<div class="row">
-			<div class="col-12">
-			<?php if (have_posts()) :
-				while (have_posts()) : the_post(); ?>
+<main>
+	<section id="blog-single" class="margin">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+				<?php if (have_posts()) :
+					while (have_posts()) : the_post(); ?>
 
-				<article id="blog-post" itemscope="" itemtype="http://schema.org/BlogPosting">
-					<h1 itemprop="name headline"><?php the_title(); ?></h1>
-					<time datetime="<?php the_time('F jS, Y') ?>" itemprop="datePublished"><small><?php the_time('F jS, Y') ?></small></time>
-					<div class="content" itemprop="articleBody"><?php the_content(); ?></div>
-				</article>
+					<article id="blog-post" itemscope="" itemtype="http://schema.org/BlogPosting">
+						<h1 itemprop="name headline"><?php the_title(); ?></h1>
+						<time datetime="<?php the_time('F jS, Y') ?>" itemprop="datePublished"><small><?php the_time('F jS, Y') ?></small></time>
+						<div class="content" itemprop="articleBody"><?php the_content(); ?></div>
+					</article>
 
-				<div class="navigation">
-					<div class="alignleft"><?php previous_post_link('&laquo; %link') ?></div>
-					<div class="alignright"><?php next_post_link('%link &raquo;') ?></div>
+					<div class="navigation">
+						<div class="alignleft"><?php previous_post_link('&laquo; %link') ?></div>
+						<div class="alignright"><?php next_post_link('%link &raquo;') ?></div>
+					</div>
+
+					<?php endwhile; ?>
+				<?php endif; ?>
 				</div>
-
-				<?php endwhile; ?>
-			<?php endif; ?>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
+</main>
 
 <?php get_footer(); ?>
